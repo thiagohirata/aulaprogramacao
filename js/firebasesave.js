64d477c1febfa46c6b686bd041a3bc8e98a268da
Thiago Hirata <hirata@devcase.com.br>
2016-08-18 18:12:46 -0300

    Aula 7

---
+++ js/firebasesave.js
@@ -0,0 +1,36 @@
+$(function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+      $('.autofirebasecontent').each(function() {
+        var contentid = $(this).attr('firebasesave-contentid');
+        if(!contentid) return;
+        var textarea = $(this);
+
+        var firebaseref = firebase.database().ref('/saveddata/' + user.uid + '/' + contentid);
+
+        firebaseref.once('value', function(snapshot) {
+          if(!snapshot.val()) return;
+          textarea.val(snapshot.val().code);
+          if(textarea.data('codemirror')) {
+            textarea.data('codemirror').setValue(snapshot.val().code);
+          }
+          textarea.addClass('firebase-loaded');
+          textarea.trigger('firebase-post-load');
+        });
+
+        textarea.on('firebase-save', function() {
+          //grava código no firebase
+          try {
+            if(textarea.data('codemirror')) {
+              firebaseref.set( {code : textarea.data('codemirror').getValue() });
+            } else {
+              firebaseref.set ({ code: textarea.val() });
+            }
+          } catch(err) {
+            console.log(err);
+          }
+        });
+      });
+    }
+  });
+});
